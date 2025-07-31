ef95f8abf6a8d19328d56b0a06d846561c7620b1
Mohaned Ahmed Shehata <mohanedahmed015529.222.00@gmail.com>
2025-07-31 20:40:21 +0300

    Fix: use secure_asset for HTTPS links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,57 +5,51 @@
     <title>Login</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="{{ asset('products/images/icons/favicon.ico') }}" />
+    <link rel="icon" type="image/png" href="{{ secure_asset('products/images/icons/favicon.ico') }}" />
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('products/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('products/vendor/bootstrap/css/bootstrap.min.css') }}">
     <!--===============================================================================================-->
     <link rel="stylesheet" type="text/css"
-        href="{{ asset('products/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+        href="{{ secure_asset('products/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('products/vendor/animate/animate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('products/vendor/animate/animate.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('products/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('products/vendor/css-hamburgers/hamburgers.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('products/vendor/select2/select2.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('products/vendor/select2/select2.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('products/css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('products/css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('products/css/main.css') }}">
     <!--===============================================================================================-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('products/css/main.css') }}">
     <!--===============================================================================================-->
 </head>
 
 <body>
 
-
-
-
     {{-- content of login and Register --}}
-
     @yield('content')
 
-
-
-
     <!--===============================================================================================-->
-    <script src="{{ asset('products/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ secure_asset('products/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="{{ asset('products/vendor/bootstrap/js/popper.js') }}"></script>
-    <script src="{{ asset('products/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ secure_asset('products/vendor/bootstrap/js/popper.js') }}"></script>
+    <script src="{{ secure_asset('products/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="{{ asset('products/vendor/select2/select2.min.js') }}"></script>
+    <script src="{{ secure_asset('products/vendor/select2/select2.min.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="{{ asset('products/vendor/tilt/tilt.jquery.min.js') }}"></script>
+    <script src="{{ secure_asset('products/vendor/tilt/tilt.jquery.min.js') }}"></script>
     <script>
         $('.js-tilt').tilt({
             scale: 1.1
         })
     </script>
     <!--===============================================================================================-->
-    <script src="{{ asset('products/js/main.js') }}"></script>
+    <script src="{{ secure_asset('products/js/main.js') }}"></script>
 
 </body>
 
