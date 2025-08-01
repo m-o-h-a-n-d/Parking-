8c2af4146490e32c1610a74f6fe531f09eb102aa
Mohaned Ahmed Shehata <mohanedahmed015529.222.00@gmail.com>
2025-08-01 04:45:21 +0300

    this is Edit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# Run Laravel setup commands
+echo "⏳ Waiting for DB to be ready..."
+
+until nc -z -v -w30 "$DB_HOST" "$DB_PORT"
+do
+  echo "Waiting for MySQL at $DB_HOST:$DB_PORT..."
+  sleep 5
+done
+
+echo "✅ DB is up!"
+
 php artisan config:cache
 php artisan migrate --force
 
-# Start PHP-FPM
 exec php-fpm
